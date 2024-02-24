5ef130fc4aac16cec9f876856e8f73d10cb5be24
Nathan Dai <nathandai2000@gmail.com>
2024-02-24 10:20:32 -0800

    Add Utils.js and update imports and state in Page.js

--- components/Page.js
+++ components/Page.js
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 
 import data from '../data/data.json';
-import Question from '@/components/Question';
+import Question from '@/components/Question/Question';
 import Navigation from '@/components/Navbar';
 
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '@/firebase/firebase';
+import { auth, db } from '@/firebase/firebase';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 
-const Page = ({data_cluster, cluster}) => {
+const Page = ({ data_cluster, cluster }) => {
 	const [questions] = useState(new Set(data[data_cluster]));
 	const [question, setQuestion] = useState(data[data_cluster][0]);
 	const [userId, setUserId] = useState();
+
+	const [completedQuestions, setCompletedQuestions] = useState([]);
+	const [missedQuestions, setMissedQuestions] = useState([]);
 
 	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
@@ -28,6 +32,62 @@ const Page = ({data_cluster, cluster}) => {
 		setQuestion(data[data_cluster][startI]);
 	}, [questions.size]);
 
+	// useEffect(() => {
+	// 	const docRef = doc(db, 'users', userId);
+	// 	const unsub = onSnapshot(docRef, (doc) => {
+	// 		if (doc.exists()) {
+	// 			const userData = doc.data();
+	// 			const completedQuestions = userData[category]['completedQuestions'];
+	// 			const missedQuestions = userData[category]['missedQuestions'];
+
+	// 			setCompletedQuestions(completedQuestions);
+	// 			setMissedQuestions(missedQuestions);
+
+	// 			// if current question is in missedQuestions, setIsBookmarked to true
+	// 			if (missedQuestions.includes(question.id)) {
+	// 				setIsBookmarked(true);
+	// 			}
+	// 		} else {
+	// 			console.log('No such document!');
+	// 		}
+	// 	});
+
+	// 	return unsub;
+	// }, [userId, question.id]);
+
+	const getNextQuestion = () => {
+		const getRandomItem = (set) => {
+			const items = Array.from(set);
+			return items[Math.floor(Math.random() * items.length)];
+		};
+
+		const difference = (a, b) => {
+			return new Set([...a].filter((x) => !b.has(x)));
+		};
+
+		const union = (a, b) => {
+			return new Set([...a, ...b]);
+		};
+
+		const randomId = getRandomItem(
+			union(
+				difference(
+					Array.from(questions).map((question) => {
+						return question.id;
+					}),
+					new Set(completedQuestions)
+				),
+				new Set(missedQuestions)
+			)
+		);
+
+		const question = Array.from(questions).find((question) => {
+			return question.id === randomId;
+		});
+
+		return question;
+	};
+
 	return (
 		<>
 			<Navigation />
@@ -36,7 +96,7 @@ const Page = ({data_cluster, cluster}) => {
 					<Question
 						question={question}
 						setQuestion={setQuestion}
-						questions={questions}
+						getNextQuestion={getNextQuestion}
 						category={cluster}
 						userId={userId}
 					/>
