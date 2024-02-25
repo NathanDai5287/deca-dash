5eb5dfd22541eea82741abb4e6815aab31e84dd5
Nathan Dai <nathandai2000@gmail.com>
2024-02-24 23:07:39 -0800

    Add QuestionHistory component to display question history

--- components/Page.js
+++ components/Page.js
@@ -1,15 +1,12 @@
 import { useState, useEffect } from 'react';
-import Image from 'next/image';
 
 import data from '../data/data.json';
 import Question from '@/components/Question/Question';
 import Navigation from '@/components/Navbar';
-
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import QuestionHistory from './QuestionHistory';
 
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/firebase/firebase';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Page = ({ data_cluster, cluster }) => {
 	const [questions] = useState(new Set(data[data_cluster]));
@@ -18,8 +15,6 @@ const Page = ({ data_cluster, cluster }) => {
 
 	const [completedQuestions, setCompletedQuestions] = useState([]);
 	const [missedQuestions, setMissedQuestions] = useState([]);
-
-	const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
 
 	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
@@ -71,44 +66,7 @@ const Page = ({ data_cluster, cluster }) => {
 
 	return (
 		<div className='flex flex-row-reverse h-screen'>
-			<Sidebar className='h-full bg-blue-50' collapsed={sidebarCollapsed}>
-				<button
-					class='bg-blue-100 hover:bg-blue-200 text-black font-semibold p-3 m-2 mb-0 rounded-2xl inline-flex items-center ease-out duration-100'
-					onClick={() => {
-						setSidebarCollapsed(!sidebarCollapsed);
-					}}
-				>
-					<Image
-						src='https://raw.githubusercontent.com/eliyantosarage/font-awesome-pro/7bd749a561c9759b66ee5189c4d48877caf00e92/fontawesome-pro-6.5.1-web/svgs/regular/sidebar-flip.svg'
-						className={'fill-current w-8' + (!sidebarCollapsed ? ' mr-2' : '')}
-						width={1}
-						height={1}
-						alt='collapse-sidebar-icon'
-					/>
-					{!sidebarCollapsed ? 'Collapse' : ''}
-				</button>
-
-				<Menu>
-					<h5 className='text-center bg-green-100 mx-1 my-2 p-3 text-gray-900 rounded-2xl font-semibold text-2xl'>
-						{!sidebarCollapsed ? (
-							'Question History'
-						) : (
-							<Image
-								src='https://raw.githubusercontent.com/eliyantosarage/font-awesome-pro/7bd749a561c9759b66ee5189c4d48877caf00e92/fontawesome-pro-6.5.1-web/svgs/solid/clock-rotate-left.svg'
-								className='fill-current w-8'
-								width={1}
-								height={1}
-								alt='question-history-icon'
-							/>
-						)}
-					</h5>
-					<MenuItem> Pie charts </MenuItem>
-					<MenuItem> Line charts </MenuItem>
-					<MenuItem> Documentation </MenuItem>
-					<MenuItem> Calendar </MenuItem>
-				</Menu>
-			</Sidebar>
-
+			{userId && <QuestionHistory questions={questions} userId={userId} category={cluster} />}
 			<div className='flex-1'>
 				<Navigation />
 				<div id='container' className='mx-auto sm:w-full md:w-1/2'>
---
+++ components/QuestionHistory.js
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react';
+
+import Image from 'next/image';
+
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+
+import { db } from '@/firebase/firebase';
+import { doc, onSnapshot } from 'firebase/firestore';
+
+const QuestionHistory = ({ questions, userId, category }) => {
+	const [completedQuestions, setCompletedQuestions] = useState([]);
+	const [missedQuestions, setMissedQuestions] = useState([]);
+
+	const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+	useEffect(() => {
+		const docRef = doc(db, 'users', userId);
+		const unsub = onSnapshot(docRef, (doc) => {
+			if (doc.exists()) {
+				const userData = doc.data();
+				const completedQuestions = userData[category]['completedQuestions'];
+				const missedQuestions = userData[category]['missedQuestions'];
+
+				setCompletedQuestions(completedQuestions);
+				setMissedQuestions(missedQuestions);
+			} else {
+				console.log('No such document!');
+			}
+		});
+
+		return unsub;
+	}, [category, userId]);
+
+	return (
+		<Sidebar className='h-full bg-blue-50' collapsed={sidebarCollapsed} width='420px'>
+			<div className='flex justify-end'>
+				<button
+					className='bg-blue-100 hover:bg-blue-200 text-black text-xl font-semibold p-3 m-2 mb-0 rounded-2xl inline-flex items-center ease-out duration-100'
+					onClick={() => {
+						setSidebarCollapsed(!sidebarCollapsed);
+					}}
+				>
+					<Image
+						src='https://raw.githubusercontent.com/eliyantosarage/font-awesome-pro/7bd749a561c9759b66ee5189c4d48877caf00e92/fontawesome-pro-6.5.1-web/svgs/regular/sidebar-flip.svg'
+						className={'fill-current w-8' + (!sidebarCollapsed ? ' mr-2' : '')}
+						// className='fill-current w-8'
+						width={1}
+						height={1}
+						alt='collapse-sidebar-icon'
+					/>
+					{!sidebarCollapsed ? 'Collapse' : ''}
+				</button>
+			</div>
+
+			<Menu>
+				<h5 className='text-center bg-green-100 mx-1 my-2 p-3 text-gray-900 rounded-2xl font-semibold text-2xl'>
+					{!sidebarCollapsed ? (
+						'Question History'
+					) : (
+						<Image
+							src='https://raw.githubusercontent.com/eliyantosarage/font-awesome-pro/7bd749a561c9759b66ee5189c4d48877caf00e92/fontawesome-pro-6.5.1-web/svgs/solid/clock-rotate-left.svg'
+							className='fill-current w-8'
+							width={1}
+							height={1}
+							alt='question-history-icon'
+						/>
+					)}
+				</h5>
+
+				{[...completedQuestions].reverse().map((questionId) => {
+					const question = Array.from(questions).find((question) => {
+						return question.id === questionId;
+					});
+
+					return (
+						<MenuItem key={questionId} className='border-2 rounded m-1 border-gray-200'>
+							{question.question}
+						</MenuItem>
+					);
+				})}
+			</Menu>
+		</Sidebar>
+	);
+};
+
+export default QuestionHistory;
