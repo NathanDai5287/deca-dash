2d82844e4d787a8f3e9a08153eefb0a59468c963
Nathan Dai <nathandai2000@gmail.com>
2024-11-08 17:40:59 -0800

    add questions

---
+++ data/processing/json_answers_to_dataframe.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+pd.read_json(r'C:\Users\natha\Programming\deca-dash\data\processing\answers.json').to_clipboard(index=False, header=False)
---
+++ data/processing/json_questions_to_dataframe.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import json
+
+pd.read_json(r'C:\Users\natha\Programming\deca-dash\data\processing\questions.json').to_clipboard(index=False, header=None)
